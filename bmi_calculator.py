a30a7a5d660a5c9b7d5fdd598ca9977779863628
Quocviet Le <quocvietlet@gmail.com>
2024-07-31 23:31:10 -0500

    added bmi function

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,16 +1,20 @@
+def bmi_calculator(w, h):
+    BMI = w * 720 / (h**2)
+    return BMI
+
+
 def BMI():
     w = float(input("Enter person's weight in pounds: "))
     h = float(input("Enter person's height in inches: "))
-    BMI = w * 720 / (h**2)
+    BMI = bmi_calculator(w, h)
+
+    print(f"BMI: {round(BMI, 2)}")
 
     if BMI >= 19 and BMI <= 26:
-        print(round(BMI, 2))
         print("within healthy range")
     elif BMI > 26:
-        print(round(BMI, 2))
         print("below healthy range")
     else:
-        print(round(BMI, 2))
         print("above healthy range")
 
 
